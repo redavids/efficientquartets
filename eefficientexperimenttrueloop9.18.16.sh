aaba1a92b4afebeff90f8126937259c16f65b20e
Ruth Davidson <redavids@ncsu.edu>
2016-09-18 20:46:48 -0700

    eeff is temporary file do not use

--- eefficientexperimenttrueloop9.18.16.sh
+++ eefficientexperimenttrueloop9.18.16.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {1..50}; do
-	~/code/wQMC/max-cut-tree qrtt=~/DATA/ASTRAL2.50TaxaForEfficientQuartets/model.50.2000000.0.000001.true/$i/truegenetrees.6.2.16.efficientWQMCquartets weights=on otre=~/DATA/ASTRAL2.50TaxaForEfficientQuartets/model.50.2000000.0.000001.true/$i/6.2.16.efficient.true.wQMC.tree
+	~/code/wQMC/max-cut-tree qrtt=/Users/ruthdavidson/code/DATA/ASTRAL2.50Taxa/model.50.2000000.0.000001.true/$i/truegenetrees weights=on otre=~/DATA/ASTRAL2.50TaxaForEfficientQuartets/model.50.2000000.0.000001.true/$i/9.18.16.all.true.wQMC.tree
 
-	~/code/compareTrees/compareTrees.missingBranchRate ~/DATA/ASTRAL2.50TaxaForEfficientQuartets/model.50.2000000.0.000001.species/$i/s_tree.trees ~/DATA/ASTRAL2.50TaxaForEfficientQuartets/model.50.2000000.0.000001.true/$i/6.2.16.efficient.true.wQMC.tree > ~/DATA/ASTRAL2.50TaxaForEfficientQuartets/model.50.2000000.0.000001.true/$i/6.2.16.efficient.missingBranchRate
+	~/code/compareTrees/compareTrees.missingBranchRate ~/DATA/ASTRAL2.50TaxaForEfficientQuartets/model.50.2000000.0.000001.species/$i/s_tree.trees ~/DATA/ASTRAL2.50TaxaForEfficientQuartets/model.50.2000000.0.000001.true/$i/9.18.16.all.true.wQMC.tree > ~/DATA/ASTRAL2.50TaxaForEfficientQuartets/model.50.2000000.0.000001.true/$i/6.2.16.efficient.missingBranchRate
 
 done
